a9f1f950519314a0ae616b415c8d3d6330beec3c
Keleigh Williams <keleighwilliams@gmail.com>
2022-08-19 12:07:40 -0400

    updating folder

--- intro-to-axios/app.js
+++ intro-to-axios/app.js
@@ -2,13 +2,18 @@ const list = document.getElementById("list")
 list.style.listStyle = "lower-roman"
 const form = document.todoform
 
+function getData(){
+    axios.get("https://api.vschool.io/keleigh/todo/")
+    .then(response => listData(response.data))
+    .catch(error => console.log(error))
+}
 ///1. GET request + add to DOM
-axios.get("https://api.vschool.io/keleigh/todo/")
-    .then(response => {
-        const responseData = response.data
+function listData(responseData){
+   
+        //const responseData = response.data
         for(let i = 0; i < responseData.length; i++){
             const li = document.createElement('li')
-            li.textContent = response.data[i].title
+            li.textContent = responseData[i].title
             li.style.color = "darkgreen"
             li.style.border = "solid salmon"
             li.style.borderRadius = "6px"
@@ -21,12 +26,12 @@ axios.get("https://api.vschool.io/keleigh/todo/")
             
 
             const newSpan = document.createElement('span')
-            newSpan.textContent = ": " + response.data[i].description
+            newSpan.textContent = ": " + responseData[i].description
             newSpan.style.margin = "4px"
             li.appendChild(newSpan)
             
             const img = document.createElement('img')
-            img.src = response.data[i].imgURL
+            img.src = responseData[i].imgURL
             li.appendChild(img)
 
             const completed = document.createElement('button')
@@ -36,7 +41,7 @@ axios.get("https://api.vschool.io/keleigh/todo/")
 
             completed.addEventListener("click", function(){
                 const update = { completed : true }
-                axios.put("https://api.vschool.io/keleigh/todo/"+response.data[i]._id, update)
+                axios.put("https://api.vschool.io/keleigh/todo/"+responseData[i]._id, update)
                 .then(response => console.log(response.data))
                 .catch(error => console.log(error))
             })
@@ -48,15 +53,14 @@ axios.get("https://api.vschool.io/keleigh/todo/")
 
              li.addEventListener("dblclick", function(){
                 list.removeChild(li)
-                    axios.delete("https://api.vschool.io/keleigh/todo/"+response.data[i]._id)
+                    axios.delete("https://api.vschool.io/keleigh/todo/"+responseData[i]._id)
                     .then(response=> console.log(response.data))
                     .catch(error => console.log(error))
             })
 
         }
          console.log(responseData)
-    })
-    .catch(error => console.log(error))
+    }
 
 
 ///2. POST Request - axios will parse the object to json/ add new data to API
@@ -71,7 +75,7 @@ form.addEventListener("submit", (event) => {
         }
 
         axios.post("https://api.vschool.io/keleigh/todo/", newTodo)
-            .then(response => console.log(response.data))
+            .then(response => getData())
             .catch(error => console.log(error))
 
             form.title.value = " "
@@ -81,6 +85,7 @@ form.addEventListener("submit", (event) => {
     })
 
 
+    getData()
 
 
 
